0991f3fe3dca7fb6728bae4d37593e5336955fb5
adruzhaev <andrewdruzhaev@gmail.com>
2022-10-27 19:49:57 +0400

    Install packages for styles

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,11 @@ module.exports = (env) => {
                     use: {
                         loader: 'babel-loader',
                     },
+                },
+                {
+                    test: /\.css$/i,
+                    exclude: /node_modules/,
+                    use: ['style-loader', 'css-loader']
                 }
             ]
         },
