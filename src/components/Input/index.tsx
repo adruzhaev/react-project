0d6c149aaeb2d67e4a85b3f0103883ed5f307767
Andrey Druzhaev <andrewdruzhaev@gmail.com>
2022-12-09 11:25:09 +0400

    Update Input component to work with forms

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,21 +1,43 @@
 import cn from 'classnames'
-import { DetailedHTMLProps, InputHTMLAttributes, ReactNode } from 'react'
+import { DetailedHTMLProps, ForwardedRef, forwardRef, InputHTMLAttributes, ReactNode } from 'react'
 import styles from './input.module.css'
 
 interface InputProps extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
     className?: string
     children?: ReactNode
     label?: string
+    error?: string
 }
 
-export const Input = ({className, label, children, ...rest}: InputProps) => {
+export const Input = forwardRef(({className, label, children, error, ...rest}: InputProps, ref: ForwardedRef<HTMLInputElement>) => {
     if (label) {
-        return <div className={styles.container}>
-            <label className={styles.label} htmlFor={rest.id}>{label}</label>
-            <input className={cn(styles.input, className)} {...rest} />
-            {children}
-        </div>
+        return (
+            error ?
+            <div className={styles.container}>
+                <div className={styles['input-container']}>
+                    <label className={styles.label} htmlFor={rest.id}>{label}</label>
+                    <input className={cn(styles.input, className)} ref={ref} {...rest} />
+                    {children}
+                </div>
+
+                {error && <p className={styles.error}>
+                    {error}
+                </p>}
+            </div> :
+            <div className={styles['input-container']}>
+                <label className={styles.label} htmlFor={rest.id}>{label}</label>
+                <input className={cn(styles.input, className)} ref={ref} {...rest} />
+                {children}
+            </div>
+        )
     }
 
-    return <input className={cn(styles.input, className)} {...rest} />
-}
+    return (
+        error ?
+        <div className={styles['input-container']}>
+            <input className={cn(styles.input, className)} ref={ref} {...rest} />
+            <p className={styles.error}>{error}</p>
+        </div> :
+        <input className={cn(styles.input, className)} ref={ref} {...rest} />
+    )
+})
