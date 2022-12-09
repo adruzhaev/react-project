6092273f74143a53d680241d1071a152bb0581d4
Andrey Druzhaev <andrewdruzhaev@gmail.com>
2022-12-09 11:42:25 +0400

    Move state for checkbox outside

--- src/components/CheckBox/index.tsx
+++ src/components/CheckBox/index.tsx
@@ -1,20 +1,18 @@
 import cn from 'classnames'
-import { DetailedHTMLProps, InputHTMLAttributes, useState } from 'react'
+import { DetailedHTMLProps, InputHTMLAttributes } from 'react'
 import styles from './CheckBox.module.css'
 
 interface CheckBoxProps extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
     className?: string
     containerClassName?: string
     label?: string
+    isChecked: boolean
 }
 
-export const CheckBox = ({className, containerClassName, label, ...rest}: CheckBoxProps) => {
-    const [isChecked, setIsChecked] = useState<boolean>(false)
-
+export const CheckBox = ({className, containerClassName, label, isChecked, ...rest}: CheckBoxProps) => {
     return <div className={cn(containerClassName, styles.container)}>
         <input
             className={cn(styles.input ,className)}
-            onChange={() => setIsChecked((prev) => !prev)}
             checked={isChecked}
             type="checkbox"
             {...rest}
